6c873de4b2a7d3a19a7b7daa5cd20ecdb565587c
e <jannatul0040@gmail.com>
2023-09-18 16:20:05 +0600

    added nav toggle and navbar animation

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,7 +1,12 @@
+import PropTypes from 'prop-types'; // ES6
 const Link = ({route}) => {
     return (
         <li className="mr-10" ><a href={route.path}></a>{route.name}</li>
     );
 };
+
+Link.propTypes = {
+    route: PropTypes.object
+}
 
 export default Link;
